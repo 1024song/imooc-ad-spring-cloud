7ac76899961b6cdba590ce7c0df38d3285d52f3e
1024song <1164453353@qq.com>
2019-06-17 21:00:27 +0800

    广告

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/interest/UnitIndex.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/interest/UnitIndex.java
@@ -2,9 +2,12 @@ package com.imooc.ad.index.interest;
 
 import com.imooc.ad.index.IndexAware;
 
+import java.util.Map;
 import java.util.Set;
 
 public class UnitIndex implements IndexAware<String, Set<Long>> {
+
+    private static Map<String,Set<Long>> itUnitMap;
     @Override
     public Set<Long> get(String key) {
         return null;
